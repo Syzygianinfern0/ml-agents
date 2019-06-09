eb19ebebd0b0ea93e63590641d185957bfbcec6f
The Syzygian Inferno <spsharan2000@gmail.com>
2019-06-09 15:10:53 +0530

    This too

--- ml-agents/setup.py
+++ ml-agents/setup.py
@@ -30,11 +30,11 @@ setup(
 
     install_requires=[
         'mlagents_envs==0.8.1',
-        'tensorflow>=1.7,<1.8',
-        'Pillow>=4.2.1',
-        'matplotlib',
-        'numpy>=1.13.3,<=1.14.5',
-        'jupyter',
+#         'tensorflow>=1.7,<1.8',
+#         'Pillow>=4.2.1',
+#         'matplotlib',
+#         'numpy>=1.13.3,<=1.14.5',
+#         'jupyter',
         'pytest>=3.2.2,<4.0.0',
         'docopt',
         'pyyaml',
@@ -42,7 +42,7 @@ setup(
         'grpcio>=1.11.0,<1.12.0',
         'pypiwin32==223;platform_system=="Windows"'],
 
-    python_requires=">=3.6,<3.7",
+#     python_requires=">=3.6,<3.7",
 
     entry_points={
         'console_scripts': [

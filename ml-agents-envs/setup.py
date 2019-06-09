831db650c154a51fd643bcebe68c2e1e26289167
The Syzygian Inferno <spsharan2000@gmail.com>
2019-06-09 15:10:02 +0530

    Added compat with my environment

--- ml-agents-envs/setup.py
+++ ml-agents-envs/setup.py
@@ -22,8 +22,8 @@ setup(
     zip_safe=False,
 
     install_requires=[
-        'Pillow>=4.2.1,<=5.4.1',
-        'numpy>=1.13.3,<=1.16.1',
+#         'Pillow>=4.2.1,<=5.4.1',
+#         'numpy>=1.13.3,<=1.16.1',
         'pytest>=3.2.2,<4.0.0',
         'protobuf>=3.6,<3.7',
         'grpcio>=1.11.0,<1.12.0',
